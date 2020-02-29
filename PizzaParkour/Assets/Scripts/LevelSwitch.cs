3c03f174b03ca0f1990d6acea0766e258faae70b
Aogu-ctrl <ogucho999@gmail.com>
2020-02-28 17:28:42 -0800

    Added basic game coding(win/lose)

---
+++ PizzaParkour/Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Death")
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (other.tag == "Customer1")
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+}
