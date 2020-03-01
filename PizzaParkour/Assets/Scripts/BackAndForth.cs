e8edbb48101f1a2ed48e47d98aed22faa1574e75
Yuvan Wickramasinghe <ywickram@usc.edu>
2020-03-01 09:00:55 +0900

    Add parking lot.

---
+++ PizzaParkour/Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+    public float movementSpeed;
+    public float distance;
+    private Vector3 origin;
+
+    private void Awake()
+    {
+        origin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sinValue = Mathf.Sin(Time.time * movementSpeed);
+        transform.position = origin + transform.forward * distance * sinValue;
+    }
+}
---
+++ PizzaParkour/Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationalSpeed;
+    private float rotation = 0f;
+
+    private void Awake()
+    {
+        rotation = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = rotation + (Time.deltaTime * rotationalSpeed) % 360f;
+        transform.rotation = Quaternion.AngleAxis(rotation, transform.forward);
+    }
+}
